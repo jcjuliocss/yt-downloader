b090aa6bbaf6a67dbf11296a5ffa472fba583497
Julio Cesar <julio.silva@softrh.com.br>
2024-05-29 08:55:53 -0300

    commit inicial interface

--- downloader.py
+++ downloader.py
@@ -1,20 +1,7 @@
 import yt_dlp
 import json
 import os
-
-def download_mp3(urls):
-    URLS = [urls]
-
-    caminho = carregar_config().get('pasta_destino', '')
-
-    ydl_opts = {
-        'format': 'bestaudio',
-        'extract_audio': True,
-        'outtmpl': caminho + '%(title)s.mp3'
-    }
-
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download(URLS)
+import flet as ft
 
 def carregar_config():
     try:
@@ -36,29 +23,110 @@ def salvar_config(config):
     arquivo_config.write(json.dumps(cfg))
     arquivo_config.close()
 
-def selecionar_pasta_destino():
-    caminho = input('Caminho: ')
+def main(page: ft.Page):
+    txt = ft.Text(value='YT Downloader', size=30)
+    page.controls.append(txt)
+    page.window_width = 1000
+    page.window_height = 400
+    page.update()
 
-    config = {'pasta_destino': '{}'.format(caminho + '/')}
+    def download_mp3(urls):
+        def progress_bar(d):
+            def fecha_alert(e):
+                alert.open = False
+                page.update()
 
-    salvar_config(config=config)
+            if d['status'] == 'finished':
+                alert = ft.AlertDialog(
+                    title=ft.Text('Download Concluído.'),
+                    actions=[
+                        ft.TextButton("Ok", on_click=fecha_alert),
+                    ],
+                    actions_alignment=ft.MainAxisAlignment.END,)
+            if d['status'] == 'downloading':
+                page.add(ft.Column([ft.Text('Baixando MP3...'), pb]))
 
-def menu():
-    print('*-----YT DOWNLOADER-----*\n')
-    print('[1] Selecionar pasta de destino\n')
-    print('[2] Baixar MP3\n')
-    print('[3] Sair\n')
+                p_cent_str = d['_percent_str']
+                p_cent = p_cent_str.replace('%','')
+                pb.value = float(p_cent)
 
-    opcao = int(input())
+                page.update()
 
-    if opcao == 1:
-        selecionar_pasta_destino()
-    elif opcao == 2:
-        url = input('URL: ')
-        download_mp3(url)
-    elif opcao == 3:
-        os._exit(0)
+        URLS = [urls]
 
-    menu()
+        caminho = carregar_config().get('pasta_destino', '')
 
-menu()
+        ydl_opts = {
+            'format': 'bestaudio',
+            'extract_audio': True,
+            'outtmpl': caminho + '%(title)s.mp3',
+            'progress_hooks': [progress_bar]
+        }
+
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download(URLS)
+
+    def atualiza_caminho(e: ft.FilePickerResultEvent):
+        txt_caminho.value = e.path
+        txt_caminho.update()
+        config = {'pasta_destino': '{}'.format(e.path + '/')}
+        salvar_config(config)
+
+    def check_url_download():
+        """Verifica se tem url digitada e faz download."""
+        if not campo_url.value:
+            def fecha_alert(e):
+                alert.open = False
+                page.update()
+
+            alert = ft.AlertDialog(
+                title=ft.Text('Insira a URL.'),
+                actions=[
+                    ft.TextButton("Ok", on_click=fecha_alert),
+                ],
+                actions_alignment=ft.MainAxisAlignment.END,)
+            page.dialog = alert
+            alert.open = True
+            page.update()
+
+            return
+
+        url = campo_url.value
+        download_mp3(urls=url)
+
+    file_picker = ft.FilePicker(on_result=atualiza_caminho)
+    page.overlay.append(file_picker)
+
+    label_caminho = ft.Text('Local de destino:')
+    caminho = carregar_config().get('pasta_destino', '')
+    txt_caminho = ft.Text(weight=ft.FontWeight.BOLD, value=caminho)
+    page.add(
+        ft.Row(
+            [
+                label_caminho,
+                txt_caminho,
+            ]
+        )
+    )
+
+    campo_url = ft.TextField(label='URL')
+    page.add(campo_url)
+
+    pb = ft.ProgressBar(width=800)
+
+    page.add(
+        ft.Row(
+            [
+                ft.ElevatedButton(
+                    "Download MP3",
+                    on_click=lambda _: check_url_download()
+                ),
+                ft.ElevatedButton(
+                    "Selecionar local",
+                    on_click=lambda _: file_picker.get_directory_path()
+                ),
+            ]
+        )
+    )
+
+ft.app(target=main)
